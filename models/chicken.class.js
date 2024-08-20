31f5d8f97434e7b81a673ea75b6326beae4934e1
EmiKilic <emirhan_kilic@outlook.de>
2024-08-20 15:52:36 +0200

    animate chicken

--- models/chicken.class.js
+++ models/chicken.class.js
@@ -1,11 +1,30 @@
 class Chicken extends MovableObject {
+IMAGES_SWIMMING = [
+  "img/2.Enemy/1.Puffer fish (3 color options)/1.Swim/1.swim1.png",
+  "img/2.Enemy/1.Puffer fish (3 color options)/1.Swim/1.swim2.png",
+  "img/2.Enemy/1.Puffer fish (3 color options)/1.Swim/1.swim3.png",
+  "img/2.Enemy/1.Puffer fish (3 color options)/1.Swim/1.swim4.png",
+  "img/2.Enemy/1.Puffer fish (3 color options)/1.Swim/1.swim5.png"
+]
+currentImage = 0;
+
   constructor() {
     super().loadImage("img/2.Enemy/1.Puffer fish (3 color options)/1.Swim/1.swim1.png");
-
+    this.loadImages(this.IMAGES_SWIMMING);
 
     this.height = 50;
     this.width = 50;
     this.x = 200 + Math.random() * 500;
     this.y = 0 + Math.random() * 300;
+    this.animate();
+  }
+
+  animate() {
+    setInterval(() => {
+      let i = this.currentImage % this.IMAGES_SWIMMING.length;
+      let path = this.IMAGES_SWIMMING[i];
+      this.img = this.ImageCache[path];
+      this.currentImage++;
+    }, 150);
   }
 }
